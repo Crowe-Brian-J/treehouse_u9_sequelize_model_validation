67d73d1eb6dbc40a8555adb4d66ca4f625cbe26c
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-29 20:34:03 -0400

    update port in app.js

--- routes.js
+++ routes.js
@@ -1,44 +1,53 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
 
 // Construct a router instance.
-const router = express.Router();
-const User = require('./models').User;
+const router = express.Router()
+const User = require('./models').User
 
 // Handler function to wrap each route.
-function asyncHandler(cb) {
+const asyncHandler = (cb) => {
   return async (req, res, next) => {
     try {
-      await cb(req, res, next);
+      await cb(req, res, next)
     } catch (error) {
       // Forward error to the global error handler
-      next(error);
+      next(error)
     }
   }
 }
 
 // Route that returns a list of users.
-router.get('/users', asyncHandler(async (req, res) => {
-  let users = await User.findAll();
-  res.json(users);
-}));
+router.get(
+  '/users',
+  asyncHandler(async (req, res) => {
+    let users = await User.findAll()
+    res.json(users)
+  })
+)
 
 // Route that creates a new user.
-router.post('/users', asyncHandler(async (req, res) => {
-  try {
-    await User.create(req.body);
-    res.status(201).json({ "message": "Account successfully created!" });
-  } catch (error) {
-    console.log('ERROR: ', error.name);
+router.post(
+  '/users',
+  asyncHandler(async (req, res) => {
+    try {
+      await User.create(req.body)
+      res.status(201).json({ message: 'Account successfully created!' })
+    } catch (error) {
+      console.log('ERROR: ', error.name)
 
-    if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
-      const errors = error.errors.map(err => err.message);
-      res.status(400).json({ errors });   
-    } else {
-      throw error;
+      if (
+        error.name === 'SequelizeValidationError' ||
+        error.name === 'SequelizeUniqueConstraintError'
+      ) {
+        const errors = error.errors.map((err) => err.message)
+        res.status(400).json({ errors })
+      } else {
+        throw error
+      }
     }
-  }
-}));
+  })
+)
 
-module.exports = router;
+module.exports = router
